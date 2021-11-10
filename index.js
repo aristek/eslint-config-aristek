e71b3bb1a6d1fd2c47ee156ba27edae64a45339c
Dmitry Semenchuk <dsemenchuk@aristeksystems.com>
2021-11-10 17:44:48 +0300

    feat: make typescript as separate config
    BREAKING CHANGE: To enable typescript support extend from aristek/typescript in your eslint config.

--- index.js
+++ index.js
@@ -1,96 +1,118 @@
-const path = require("path");
-const fs = require("fs");
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
-const projectRoot = resolveApp(".");
-const tsConfig = resolveApp("tsconfig.json");
-
-const baseConfig = require.resolve("./base");
-
 module.exports = {
-  overrides: [
-    {
-      // Extend config for js(x) files.
-      files: ["**/*.js?(x)"],
-      // Default eslint parser for JavaScript.
-      parser: "@babel/eslint-parser",
-      // Additionally, resolve imports from ts(x) files into js(x).
-      extends: [baseConfig, "plugin:import/typescript"],
-      rules: {
-        // Disable for 'js' files in case of mixed configs as described in docs.
-        // https://github.com/typescript-eslint/typescript-eslint/issues/851
-        "@typescript-eslint/explicit-function-return-type": "off",
-      },
-    },
-    {
-      // Extend config for ts(x) files.
-      files: ["**/*.ts?(x)"],
-      // Default eslint parser for TypeScript.
-      // https://github.com/palantir/tslint/issues/4534
-      parser: "@typescript-eslint/parser",
-      // Find project's root and project's `tsconfig.json` for parser.
-      parserOptions: { project: tsConfig, tsconfigRootDir: projectRoot },
-      // Extend default config with typescript and prettier in right order.
-      extends: [
-        baseConfig,
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/typescript",
-        "prettier",
-        "prettier/prettier",
-        "prettier/react",
-        "prettier/@typescript-eslint",
-      ],
-      // TypeScript specific rules.
-      rules: {
-        // Override again because we extend `prettier` after `baseConfig`.
-        curly: ["error", "all"],
-        // Do not warn on `d.ts` references.
-        "spaced-comment": ["error", "always", { markers: ["/"] }],
-        // Typescript is used for type checking.
-        "consistent-return": "off",
-        // Allow ts(x) extensions for files with JSX.
-        "react/jsx-filename-extension": [
-          "error",
-          { extensions: [".js", ".jsx", ".tsx"] },
-        ],
-        // TypeScript is used for type checking.
-        "react/prop-types": "off",
-        // TypeScript is used for type checking.
-        "react/forbid-prop-types": "off",
-        // Do not throw error because we prefer to use type inheritance.
-        "@typescript-eslint/explicit-function-return-type": "off",
-        // Ignore unused vars for edge case (rest props)
-        // e.g. { className, ...rest }
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { ignoreRestSiblings: true },
-        ],
-        // We prefer not to use classes so this rule is useless.
-        "@typescript-eslint/unbound-method": "off",
-        // Any is the only option in some cases.
-        "@typescript-eslint/no-explicit-any": "off",
-        // Nothing bad in empty function.
-        "@typescript-eslint/no-empty-function": [
-          "error",
-          { allow: ["arrowFunctions", "functions", "methods"] },
-        ],
-        // Disable for compatibility with `@typescript-eslint/no-use-before-define`.
-        "no-use-before-define": "off",
-        // Extend `no-use-before-define` to support TypeScript.
-        "@typescript-eslint/no-use-before-define": "error",
-        // There is no need to enforce double typing because of type inheritance.
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        // Disable for compatibility with `@typescript-eslint/no-shadow`.
-        "no-shadow": "off",
-        // Nothing bad in type parameter shadow.
-        "@typescript-eslint/no-shadow": [
-          "error",
-          {
-            ignoreFunctionTypeParameterNameValueShadow: true,
-          },
+  // Make this config default globally.
+  root: true,
+  // Default eslint parser for JavaScript.
+  parser: "@babel/eslint-parser",
+  // Remove warning for config files in root.
+  parserOptions: { requireConfigFile: false },
+  // Our eslint config based mainly on `airbnb` code style.
+  // https://github.com/airbnb/javascript
+  extends: ["airbnb", "airbnb/hooks", "prettier"],
+  // Set of eslint plugins for better development experience.
+  plugins: ["import", "jsx-a11y", "react", "react-hooks", "only-warn"],
+  // Support `browser` and `jest` in addition to `airbnb` envs.
+  env: { browser: true, jest: true },
+  // Resolve absolute imports from node_modules and src.
+  settings: {
+    "import/resolver": { node: { moduleDirectory: ["node_modules", "src"] } },
+  },
+  // Default rules for all project files.
+  rules: {
+    // Disallows providing the 10 radix by default.
+    radix: ["error", "as-needed"],
+    // Forces using all curly braces.
+    curly: ["error", "all"],
+    // 'require' is used only in config files (e.g. rollup).
+    "global-require": "off",
+    // Disable linebreak style to prevent conflicts different environments.
+    "linebreak-style": "off",
+    // Keep line between class members except of simple class properties
+    // e.g. class variables or component state.
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    // There is no difference except `allowTernary` option.
+    // We prefer to use `if shorthands` for logic operations.
+    // @example condition ? method1() : method2()
+    "no-unused-expressions": ["error", { allowTernary: true }],
+    // Forbid empty if-else blocks, but allow empty `try {...} catch(e) {}`.
+    "no-empty": ["error", { allowEmptyCatch: true }],
+    // Extend airbnb rule to remove eslint warning for immer usage also.
+    "no-param-reassign": [
+      "error",
+      {
+        props: true,
+        ignorePropertyModificationsForRegex: [
+          "acc", // for reduce accumulators
+          "accumulator", // for reduce accumulators
+          "e", // for e.returnvalue
+          "ctx", // for Koa routing
+          "context", // for Koa routing
+          "req", // for Express requests
+          "request", // for Express requests
+          "res", // for Express responses
+          "response", // for Express responses
+          "$scope", // for Angular 1 scopes
+          "staticContext", // for ReactRouter context
+          "draft", // for immer
         ],
       },
-    },
-  ],
+    ],
+    // Stylistic preference.
+    "import/prefer-default-export": "off",
+    // Allow dev dependencies import for config, test, storybook files.
+    // Also ignore `src/setupTests` file for jest setup.
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.config.*",
+          "**/*.test.*",
+          "**/*.stories.*",
+          "src/setupTests.*",
+        ],
+      },
+    ],
+    // No need to specify extensions if files are named correctly.
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      { js: "never", jsx: "never", ts: "never", tsx: "never", mjs: "never" },
+    ],
+    // This rule was deprecated in v6.1.0. It will no longer be maintained.
+    // Use label-has-associated-control instead.
+    "jsx-a11y/label-has-for": "off",
+    // Disable because of false positive cases with dynamic alt text.
+    "jsx-a11y/img-redundant-alt": "off",
+    // We must provide this rule with `htmlFor` attribute (by default)
+    // because this one was broken in airbnb config by `labelAttributes: []`.
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      { labelAttributes: ["htmlFor"] },
+    ],
+    // 'defaultProps' on function components is deprecated by React (https://github.com/facebook/react/pull/16210).
+    "react/require-default-props": [
+      "error",
+      { ignoreFunctionalComponents: true },
+    ],
+    // Allow both `.js` and `.jsx` extensions.
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    // There is no need to use alternatives everywhere like `shape`.
+    // Sometimes we just need to know that this is an array in container for example.
+    // We do not do any manipulations with array props and just pass it into the component.
+    "react/forbid-prop-types": "off",
+    // Disable for our own risk without any grounding for a while.
+    // It's also unnecessary for TypeScript users because all props are checking via types.
+    "react/jsx-props-no-spreading": "off",
+    // Disable this rule because it causes an error with special html chars
+    // on some environments (e.g. Windows).
+    "react/jsx-one-expression-per-line": "off",
+    // We use both variants depending on each case.
+    "react/state-in-constructor": "off",
+    // Use destructing only in render for `props` and `state`.
+    // For handlers we use destruction only if needed.
+    "react/destructuring-assignment": "off",
+  },
 };
---
+++ typescript.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const fs = require("fs");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+const projectRoot = resolveApp(".");
+const tsConfig = resolveApp("tsconfig.json");
+
+const baseConfig = require.resolve("./base");
+
+module.exports = {
+  overrides: [
+    {
+      // Extend config for js(x) files.
+      files: ["**/*.js?(x)"],
+      // Default eslint parser for JavaScript.
+      parser: "@babel/eslint-parser",
+      // Additionally, resolve imports from ts(x) files into js(x).
+      extends: [baseConfig, "plugin:import/typescript"],
+      rules: {
+        // Disable for 'js' files in case of mixed configs as described in docs.
+        // https://github.com/typescript-eslint/typescript-eslint/issues/851
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+    {
+      // Extend config for ts(x) files.
+      files: ["**/*.ts?(x)"],
+      // Default eslint parser for TypeScript.
+      // https://github.com/palantir/tslint/issues/4534
+      parser: "@typescript-eslint/parser",
+      // Find project's root and project's `tsconfig.json` for parser.
+      parserOptions: { project: tsConfig, tsconfigRootDir: projectRoot },
+      // Extend default config with typescript and prettier in right order.
+      extends: [
+        baseConfig,
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "prettier",
+      ],
+      // TypeScript specific rules.
+      rules: {
+        // Override again because we extend `prettier` after `baseConfig`.
+        curly: ["error", "all"],
+        // Do not warn on `d.ts` references.
+        "spaced-comment": ["error", "always", { markers: ["/"] }],
+        // Typescript is used for type checking.
+        "consistent-return": "off",
+        // Allow ts(x) extensions for files with JSX.
+        "react/jsx-filename-extension": [
+          "error",
+          { extensions: [".js", ".jsx", ".tsx"] },
+        ],
+        // TypeScript is used for type checking.
+        "react/prop-types": "off",
+        // TypeScript is used for type checking.
+        "react/forbid-prop-types": "off",
+        // Do not throw error because we prefer to use type inheritance.
+        "@typescript-eslint/explicit-function-return-type": "off",
+        // Ignore unused vars for edge case (rest props)
+        // e.g. { className, ...rest }
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { ignoreRestSiblings: true },
+        ],
+        // We prefer not to use classes so this rule is useless.
+        "@typescript-eslint/unbound-method": "off",
+        // Any is the only option in some cases.
+        "@typescript-eslint/no-explicit-any": "off",
+        // Nothing bad in empty function.
+        "@typescript-eslint/no-empty-function": [
+          "error",
+          { allow: ["arrowFunctions", "functions", "methods"] },
+        ],
+        // Disable for compatibility with `@typescript-eslint/no-use-before-define`.
+        "no-use-before-define": "off",
+        // Extend `no-use-before-define` to support TypeScript.
+        "@typescript-eslint/no-use-before-define": "error",
+        // There is no need to enforce double typing because of type inheritance.
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        // Disable for compatibility with `@typescript-eslint/no-shadow`.
+        "no-shadow": "off",
+        // Nothing bad in type parameter shadow.
+        "@typescript-eslint/no-shadow": [
+          "error",
+          {
+            ignoreFunctionTypeParameterNameValueShadow: true,
+          },
+        ],
+      },
+    },
+  ],
+};
